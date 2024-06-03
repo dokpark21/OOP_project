defe39d24fe42dfcec05883075ba2ec937749406
BEB 09 박상현 <psh03225@naver.com>
2024-06-03 21:27:44 +0900

    ADD: DB Connect Test

--- src/main/java/com/example/server/ServerApplication.java
+++ src/main/java/com/example/server/ServerApplication.java
@@ -2,8 +2,10 @@ package com.example.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.example.server.repository")
 public class ServerApplication {
 
     public static void main(String[] args) {
