2787b88c3ebc058beb42a9245e194a64b863ea97
BEB 09 박상현 <psh03225@naver.com>
2024-06-07 13:27:08 +0900

    ADD: getAllComic, getAllSceneOfComic

--- src/main/java/com/example/server/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.server.repository;
-
-import com.example.server.model.Role;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RoleRepository extends MongoRepository<Role, String> {
-    Role findByName(String name);
-}
