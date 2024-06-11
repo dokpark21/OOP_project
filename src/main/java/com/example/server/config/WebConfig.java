1ca0d71ef97d275960649d922b2b7a8e7603fc6f
BEB 09 박상현 <psh03225@naver.com>
2024-06-11 12:18:22 +0900

    ADD: CORS

--- src/main/java/com/example/server/config/WebConfig.java
+++ src/main/java/com/example/server/config/WebConfig.java
@@ -1,5 +1,7 @@
+// src/main/java/com/example/server/config/WebConfig.java
 package com.example.server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,8 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8080")
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3001") // React 앱의 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
